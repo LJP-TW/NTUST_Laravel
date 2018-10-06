5070ca16d310e82e5557c380319bf7433161a98a
LJP-TW <accr94238@gmail.com>
2018-10-06 15:47:29 +0800

    Build Router and HomeController

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index');
+
+Route::get('/hello', function () {
+    return view('hello');
+});
+
+Route::get('/user/{name?}', function ($name = 'Nobody') {
+    /*
+    後面接問號代表參數可給可不給
+    但那就要在 function parameter 給預設值
+    */
+    return 'Hi, ' . strtoupper($name);
 });
